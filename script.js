f37735bc513ca68a680d5c12a96e9192561e136f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-04-03 18:22:12 -0500

    keeping score

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ newParagraph.textContent = "This is a new paragraph added with DOM manipulation.
 
 myDiv.appendChild(newParagraph)
 
+let score = 0;
+
+let numberOfPoints = 10;
+
 quizForm.addEventListener("submit", (event) => {
 
     console.log("event parameter: ", event)
@@ -20,11 +24,16 @@ quizForm.addEventListener("submit", (event) => {
 
     const userAnswer = document.getElementById("answer").value;
     const resultDiv = document.getElementById("result");
+    const scoreDiv = document.getElementById("score")
     console.log("userAnswer:", userAnswer)
 
     if (userAnswer.toLowerCase() == "austin") {
         resultDiv.textContent = "Correct!"
+        score = score + numberOfPoints;
+        scoreDiv.textContent = score;
     } else {
         resultDiv.textContent = "Incorrect!"
+        score = score - numberOfPoints;
+        scoreDiv.textContent = score;
     }
 })
