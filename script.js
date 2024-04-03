ca5b2250cb8833099bd44ca8458273f6b8c0ff3f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-04-03 18:06:21 -0500

    form that renders question and compares answer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const greetingNode = document.getElementById('greeting')
 const myDiv = document.getElementById("myDiv")
+const quizForm = document.getElementById('quizForm')
 
 console.log("greeting Node:", greetingNode)
 console.log("my div: ", myDiv)
@@ -11,3 +12,19 @@ const newParagraph = document.createElement("p")
 newParagraph.textContent = "This is a new paragraph added with DOM manipulation."
 
 myDiv.appendChild(newParagraph)
+
+quizForm.addEventListener("submit", (event) => {
+
+    console.log("event parameter: ", event)
+    event.preventDefault()
+
+    const userAnswer = document.getElementById("answer").value;
+    const resultDiv = document.getElementById("result");
+    console.log("userAnswer:", userAnswer)
+
+    if (userAnswer.toLowerCase() == "austin") {
+        resultDiv.textContent = "Correct!"
+    } else {
+        resultDiv.textContent = "Incorrect!"
+    }
+})
